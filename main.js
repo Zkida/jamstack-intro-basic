679786baa2f9e0b73a70b52dd93d5848213342ca
Joseph Ruano <seph1603@gmail.com>
2019-10-29 10:53:52 -0500

    My 1st JAMStack site

---
+++ main.js
@@ -0,0 +1,27 @@
+const listRepos = async username => {
+	const repos = await fetch(
+		`https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+	)
+	.then(res => res.json())
+	.catch(error => console.error(error));
+
+	const markup = repos
+	.map(
+		repo => /* html */`
+		<li>
+			<a href="${repo.html_url}">${repo.name}</a>
+			(* ${repo.stargazers_count})
+		</li>
+		`
+	)
+	.join('');
+
+	const content = document.getElementById('content');
+
+	content.innerHTML = /* html */ `
+	<ul>${markup}</ul>
+	`;
+}
+
+listRepos('seph1603');
+
